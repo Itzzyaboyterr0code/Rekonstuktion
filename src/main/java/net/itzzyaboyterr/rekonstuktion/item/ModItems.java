04361c5ad17dd85f4d01df9004b39fc669a53954
jokarm <itzzyaboyterr@protonmail.com>
2022-05-31 18:39:51 +0200

    Preparing things to make an item.

--- src/main/java/net/itzzyaboyterr/rekonstuktion/item/ModItems.java
+++ src/main/java/net/itzzyaboyterr/rekonstuktion/item/ModItems.java
@@ -1,11 +1,17 @@
 package net.itzzyaboyterr.rekonstuktion.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.itzzyaboyterr.rekonstuktion.Rekonstuktion;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+    public static final Item ROCK_SHARD = registerItem("rock_shard",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Rekonstuktion.MOD_ID, name), item);
