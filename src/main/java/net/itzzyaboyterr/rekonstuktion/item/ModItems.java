f2a34afa1c89dd3814bb1f3129b17a6561a0e9f2
jokarm <itzzyaboyterr@protonmail.com>
2022-05-31 18:27:40 +0200

    Preparing item file to make an item.

--- src/main/java/net/itzzyaboyterr/rekonstuktion/item/ModItems.java
+++ src/main/java/net/itzzyaboyterr/rekonstuktion/item/ModItems.java
@@ -1,4 +1,17 @@
 package net.itzzyaboyterr.rekonstuktion.item;
 
+import net.itzzyaboyterr.rekonstuktion.Rekonstuktion;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class ModItems {
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(Rekonstuktion.MOD_ID, name), item);
+    }
+    
+    public static void registerModItems() {
+        Rekonstuktion.LOGGER.info("Registering Mod Items for " + Rekonstuktion.MOD_ID);
+    }
 }
