24528964cb19709813efcb6cec3cd5d448715bbe
jokarm <itzzyaboyterr@protonmail.com>
2022-06-01 17:02:46 +0200

    Block almost done

--- src/main/java/net/itzzyaboyterr/rekonstuktion/block/ModBlocks.java
+++ src/main/java/net/itzzyaboyterr/rekonstuktion/block/ModBlocks.java
@@ -1,8 +1,10 @@
 package net.itzzyaboyterr.rekonstuktion.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.itzzyaboyterr.rekonstuktion.Rekonstuktion;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,6 +12,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
+
+    public static final Block OCTA_WOOL = registerBlock("octa_wool",
+            new Block(FabricBlockSettings.of(Material.WOOL).strength(2f)), ItemGroup.MISC);
+
+
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
