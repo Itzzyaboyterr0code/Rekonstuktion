0dea5a7919ffbf1ae2545484a46932ed604912c4
jokarm <itzzyaboyterr@protonmail.com>
2022-06-01 16:08:11 +0200

    Block Progress

--- src/main/java/net/itzzyaboyterr/rekonstuktion/block/ModBlocks.java
+++ src/main/java/net/itzzyaboyterr/rekonstuktion/block/ModBlocks.java
@@ -1,10 +1,25 @@
 package net.itzzyaboyterr.rekonstuktion.block;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.itzzyaboyterr.rekonstuktion.Rekonstuktion;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
+    private static Block registerBlock(String name, Block block, ItemGroup group) {
+        registerBlockItem(name, block, group);
+        return  Registry.register(Registry.BLOCK, new Identifier(Rekonstuktion.MOD_ID, name), block);
+    }
 
+    private static Item registerBlockItem(String name, Block block, ItemGroup group) {
+        return Registry.register(Registry.ITEM, new Identifier(Rekonstuktion.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(group)));
+    }
 
     public static void registerModBlocks() {
         Rekonstuktion.LOGGER.info("Registering ModBlocks for " + Rekonstuktion.MOD_ID);
